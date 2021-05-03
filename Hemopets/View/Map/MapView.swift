65ad1bb4098dc9e4e070a309d49bcc277ae8baab
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-03 17:03:41 -0300

    Updated map zoom to be closer to the user

--- Hemopets/View/Map/MapView.swift
+++ Hemopets/View/Map/MapView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
     
     let locationFetcher = LocationFetcher()
     
