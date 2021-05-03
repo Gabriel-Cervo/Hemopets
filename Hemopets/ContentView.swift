7d80f0d41ffe8a595ca9ae9c7cf1a6944d999588
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-03 16:10:12 -0300

    Envio inicial dos arquivos

---
+++ Hemopets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hemopets
+//
+//  Created by Joao Gabriel Dourado Cervo on 03/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hemopets/HemopetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HemopetsApp.swift
+//  Hemopets
+//
+//  Created by Joao Gabriel Dourado Cervo on 03/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HemopetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
