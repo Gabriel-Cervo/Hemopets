eb1837ff95b09a0c112bf80ce7e852a718e137ee
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-06 17:08:23 -0300

    Att location details according to designer

--- Hemopets/View/Map/LocationDetails.swift
+++ Hemopets/View/Map/LocationDetails.swift
@@ -16,13 +16,29 @@ struct LocationDetails: View {
 
             VStack(alignment: .leading) {
                 
+                HStack {
+                    Spacer()
+                    RoundedRectangle(cornerRadius: 0)
+                        .frame(width: 90, height: 1)
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .padding(.bottom, 50)
+                
+                HStack {
+                    Spacer()
+                    TitleView(text: "Detalhes")
+                    Spacer()
+                }
+                .padding(.bottom, 10)
+                
                 HemocenterImage(imageName: "testeHemo")
                 
                 VStack(alignment: .leading) {
                     SubtitleView(text: name)
                         .padding(.vertical, 15)
                     
-                    FieldView(iconName: "pencil", fieldName: "Endereço:", fieldDescription: "R. Dr. Florêncio Ygartua, 429 - Moinhos de Vento, Porto Alegre - RS, 90430-010")
+                    FieldView(iconName: "location.circle.fill", fieldName: "Endereço:", fieldDescription: "R. Dr. Florêncio Ygartua, 429 - Moinhos de Vento, Porto Alegre - RS, 90430-010")
                     
                     FieldView(iconName: "clock.fill", fieldName: "Horário:", fieldDescription: "09:00 - 18:30 (segunda a sexta)")
                     
@@ -30,6 +46,8 @@ struct LocationDetails: View {
                 }
                 
                 .padding(.leading, 10)
+                
+                Spacer()
             }
             .padding()
         }
