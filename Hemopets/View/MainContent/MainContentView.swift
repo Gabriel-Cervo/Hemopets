ae7c72593056e4825443c9f44b4e3aee86f9ab71
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-07 10:26:51 -0300

    Updated redirection page from fourth onboarding page

---
+++ Hemopets/View/MainContent/MainContentView.swift
@@ -0,0 +1,20 @@
+//
+//  MainContentView.swift
+//  Hemopets
+//
+//  Created by Joao Gabriel Dourado Cervo on 07/05/21.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        MapView()
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
